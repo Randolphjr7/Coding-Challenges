1aabf971b55763360340ed0418c7acd1c30d40cd
Randolph Amaya <38093455+Randolphjr7@users.noreply.github.com>
2019-12-16 17:59:22 -0500

    Create camelCase.js

---
+++ camelCase.js
@@ -0,0 +1,15 @@
+/* Complete the solution so that the function will break up camel casing, 
+using a space between words.
+
+Example
+solution('camelCasing') // => should return 'camel Casing */
+
+
+function solution(string) {
+  return string.split('').map(function(letter) {
+    return letter === letter.toUpperCase() ? (" " + letter) : letter
+  }).join('');
+}
+
+
+solution('camelCasing')
