5c66ecc4c78c0aa04ed5917d3d17242b344f9226
Randolph Amaya <38093455+Randolphjr7@users.noreply.github.com>
2020-01-03 17:36:08 -0500

    Update who-likes-it.js

--- who-likes-it.js
+++ who-likes-it.js
@@ -11,6 +11,7 @@ likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
 likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 other like this" 
 */
 
+// My Solution
 function likes(names){
 
   let result = "no one likes this"
@@ -47,8 +48,9 @@ let test5 = ["alex", "peter", "john", "randy"];
 likes(test4);
 
 
-// Best Practice 
+ 
 /*
+// Best Practice
 function likes(names) {
   names = names || [];
   switch(names.length){
